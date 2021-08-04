bfc1f36e7ad5b2e8aef0d0ab5e937a6a7b644fa8
송지연 <6loutlside@naver.com>
2021-08-04 16:28:09 +0900

    [BE][Fix][#S05P13B306-30] API 수정
    오류나던 API 수정

--- back/src/main/java/com/ppakgom/api/controller/UserController.java
+++ back/src/main/java/com/ppakgom/api/controller/UserController.java
@@ -38,7 +38,6 @@ public class UserController {
 		/* 임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음. */
 		User user = userService.createUser(registerInfo, thumbnail);
 		
-		
 		if(user == null) // 회원가입이 실패한 경우
 			return ResponseEntity.status(400).body(BaseResponseBody.of(400, "다시 시도해주세요."));
 		
@@ -46,13 +45,13 @@ public class UserController {
 		return ResponseEntity.status(201).body(BaseResponseBody.of(201, "가입 승인 완료"));
 	}
 	
-	@GetMapping("/${name}/name")
+	@GetMapping("/{nickname}/name")
 	@ApiOperation(value = "닉네임 중복 체크", notes = "<strong>닉네임</strong>중복 체크를 한다.")
 	public ResponseEntity<? extends BaseResponseBody> checkDuplicateName(
-			@PathVariable @ApiParam(value="중복확인 Name", required = true) String name){
+			@PathVariable(value = "nickname") @ApiParam(value="중복확인 Name", required = true) String nickname){
 		
 		// 만약 해당 닉네임이 있다면 400에러 발생
-		if(userService.checkDuplicateName(name))
+		if(!userService.checkName(nickname))
 			return ResponseEntity.status(400).body(BaseResponseBody.of(400, "이미 존재하는 닉네임입니다."));
 		
 		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "사용 가능한 닉네임입니다."));
