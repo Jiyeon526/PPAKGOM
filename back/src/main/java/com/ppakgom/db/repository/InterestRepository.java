98a5f7e690de4a92b875df67670325395e17724d
송지연 <6loutlside@naver.com>
2021-08-04 16:19:30 +0900

    [BE][Rename][#S05P13B306-27] 어노테이션 수정

--- back/src/main/java/com/ppakgom/db/repository/InterestRepository.java
+++ back/src/main/java/com/ppakgom/db/repository/InterestRepository.java
@@ -3,13 +3,15 @@ package com.ppakgom.db.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.ppakgom.db.entity.Interest;
 
+@Repository
 public interface InterestRepository extends JpaRepository<Interest, Long> {
 
 	Optional<Interest> findByName(String s);
 	void save(String s);
-	Long findCountId();
-	
+
 }
