f5b82ef57ed81ca8c85ebfa5a8672e650eb50be6
송지연 <6loutlside@naver.com>
2021-08-04 22:31:20 +0900

    [BE][Fix][#S05P13B306-27] save 함수 삭제
    생성자 만들어서 해결

--- back/src/main/java/com/ppakgom/db/repository/InterestRepository.java
+++ back/src/main/java/com/ppakgom/db/repository/InterestRepository.java
@@ -2,8 +2,8 @@ package com.ppakgom.db.repository;
 
 import java.util.Optional;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ppakgom.db.entity.Interest;
@@ -12,6 +12,5 @@ import com.ppakgom.db.entity.Interest;
 public interface InterestRepository extends JpaRepository<Interest, Long> {
 
 	Optional<Interest> findByName(String s);
-	void save(String s);
 
 }
