9a5d06a33466522512b883490193f4283b1d84eb
송지연 <6loutlside@naver.com>
2021-08-07 09:27:52 +0900

    [BE][Feat][#S05P13B306-36] 쿼리문 추가

--- back/src/main/java/com/ppakgom/db/repository/UserInterestRepository.java
+++ back/src/main/java/com/ppakgom/db/repository/UserInterestRepository.java
@@ -2,8 +2,12 @@ package com.ppakgom.db.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ppakgom.db.entity.UserInterest;
@@ -13,5 +17,10 @@ public interface UserInterestRepository extends JpaRepository<UserInterest, Long
 	
 	@Query(value = "SELECT INTEREST_ID FROM USER_INTEREST WHERE USER_ID = ?1",nativeQuery = true) 
 	List<Long> findByInterestId(Long userid);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "delete from user_interest where user_id= :user_id and interest_id= :interest_id",nativeQuery = true)
+	void deleteUserInterest(@Param("user_id") Long user_id,@Param("interest_id") Long interest_id);
 
 }
