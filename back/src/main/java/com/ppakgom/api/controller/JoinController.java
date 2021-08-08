ccfc7688e73e633b663c59da35919c93b32b231c
송지연 <6loutlside@naver.com>
2021-08-08 23:36:39 +0900

    [BE][Feat] 신청 취소 요청 컨트롤러 추가

--- back/src/main/java/com/ppakgom/api/controller/JoinController.java
+++ back/src/main/java/com/ppakgom/api/controller/JoinController.java
@@ -4,16 +4,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ppakgom.api.request.JoinApplyCancelReq;
 import com.ppakgom.api.response.JoinApplyListRes;
 import com.ppakgom.api.service.JoinService;
+import com.ppakgom.common.model.response.BaseResponseBody;
+import com.ppakgom.db.entity.StudyApply;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @Api(value="가입 API", tags = {"Join"})
 @RestController
@@ -30,5 +36,21 @@ public class JoinController {
 		List<JoinApplyListRes> res = joinService.getJoinApplyList(user_id);
 					
 		return ResponseEntity.status(200).body(res);
+	}
+	
+	@DeleteMapping("/response/cancel/{userid}")
+	@ApiOperation(value="가입 신청 취소", notes="사용자의 가입 신청 취소")
+	public ResponseEntity<? extends BaseResponseBody> joinApplyCancel(@PathVariable Long userid, 
+			@RequestBody JoinApplyCancelReq joinApplyCancelReq) {
+		
+		// 해당 study_apply 정보 가져오기(어차피 하나밖에 없다)
+		StudyApply studyApply = joinService.getStudyApply(userid, joinApplyCancelReq);
+		
+		if(studyApply == null)
+			return ResponseEntity.status(400).body(BaseResponseBody.of(400, "다시 시도해 주세요."));
+		
+		joinService.deleteJoinApplyCancel(studyApply); // 해당 튜플 삭제
+		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "가입 취소 완료"));
+
 	}
 }
