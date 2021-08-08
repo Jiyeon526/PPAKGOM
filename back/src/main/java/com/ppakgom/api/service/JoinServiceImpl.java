6ee52b5908ab93a370df9e7d94a6d09106ba6d4c
송지연 <6loutlside@naver.com>
2021-08-08 22:56:32 +0900

    [BE][Feat][#S05P13B306-42] 가입요청현황 서비스 객체 생성

---
+++ back/src/main/java/com/ppakgom/api/service/JoinServiceImpl.java
@@ -0,0 +1,49 @@
+package com.ppakgom.api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ppakgom.api.response.JoinApplyListRes;
+import com.ppakgom.db.entity.Study;
+import com.ppakgom.db.entity.StudyApply;
+import com.ppakgom.db.repository.StudyApplyRepository;
+import com.ppakgom.db.repository.StudyRepository;
+
+@Service("JoinService")
+public class JoinServiceImpl implements JoinService {
+	
+	@Autowired
+	StudyApplyRepository studyApplyRepository;
+	
+	@Autowired
+	StudyRepository studyRepository;
+	
+	@Override
+	public List<JoinApplyListRes> getJoinApplyList(Long user_id) {
+		
+		List<StudyApply> studyApply = studyApplyRepository.findBySender_Id(user_id); // 가입 요청 리스트
+		List<JoinApplyListRes> res = new ArrayList<>();
+		
+		if(studyApply == null) return null; // 가입 요청 없으면 null 반환
+		
+		for(StudyApply study : studyApply) {
+			if(study.is_join() == false) continue; // 초대이면 넘기기
+			JoinApplyListRes list = new JoinApplyListRes();
+			// 스터디 정보 가져오기
+			Optional<Study> s = studyRepository.findById(study.getStudy().getId());
+			if(!s.isPresent()) continue;
+			list.setStudy_id(s.get().getId()); // 스터디 순번 저장
+			list.setStudy_name(s.get().getName()); // 스터디 이름 저장
+			list.setOwner_name(s.get().getUser().getName()); // 스터디 방장 닉네임 저장
+			list.setState(study.getState()); // 상태 저장
+			res.add(list);
+		}
+		
+		return res;
+	}
+
+}
