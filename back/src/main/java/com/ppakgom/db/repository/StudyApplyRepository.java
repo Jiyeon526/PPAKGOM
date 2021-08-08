454b409c0eb13499afaa7775ea4ce6de24787106
송지연 <6loutlside@naver.com>
2021-08-08 22:57:45 +0900

    [BE][Feat][#S05P13B306-42] study_apply repository 생성

---
+++ back/src/main/java/com/ppakgom/db/repository/StudyApplyRepository.java
@@ -0,0 +1,15 @@
+package com.ppakgom.db.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.ppakgom.db.entity.StudyApply;
+
+public interface StudyApplyRepository extends JpaRepository<StudyApply, Long> {
+
+//	@Query("SELECT * FROM STUDY_APPLY WHERE IS_JOIN = 1 AND SENDER_ID = ?1",nativeQuery = true)
+	List<StudyApply> findBySender_Id(Long sender_id);
+
+}
