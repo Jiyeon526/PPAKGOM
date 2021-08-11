fd7e79a9a17c4119c545237a74d23bfbcc99686a
kwanggyo <dlrhkdry29@gmail.com>
2021-08-11 17:37:25 +0900

    [FE][Feat][#S05P13B306-83] 스터디 달력 컴포넌트 기본
    1. npm install v-calendar@next로 Vue3 calendar를 설치했다.
    2. 달력 나오는 것까지 구현

--- frontend/src/views/studydetail/studyschedule.vue
+++ frontend/src/views/studydetail/studyschedule.vue
@@ -1,20 +1,41 @@
 <template>
   <h2>스터디 일정</h2>
+  <Calendar
+    style="width:700px"
+    :attributes="state.attrs"
+    v-model="state.date"
+  />
+  <!-- <DatePicker v-model="date" /> -->
 </template>
 
 <script>
-import { onMounted } from 'vue'
+import { Calendar, DatePicker } from 'v-calendar';
+import { onMounted, reactive } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
   name: 'studyschedule',
+  components: {
+    Calendar,
+    DatePicker,
+  },
   setup () {
     const store = useStore()
-
+    const state = reactive({
+      attrs: [
+        {
+          key: 'today',
+          highlight: true,
+          dates: new Date(),
+        },
+      ],
+      date: new Date(),
+    })
     // 페이지 진입시 불리는 훅
     onMounted (() => {
 
     })
+    return { state }
   }
 }
 </script>
