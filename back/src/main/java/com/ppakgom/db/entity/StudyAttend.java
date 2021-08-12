69355da232bf1a32a071ec489b7c45685061096e
송지연 <6loutlside@naver.com>
2021-08-12 14:27:28 +0900

    [BE][Feat][#S05P13B306-103] study attend 테이블 생성

---
+++ back/src/main/java/com/ppakgom/db/entity/StudyAttend.java
@@ -0,0 +1,29 @@
+package com.ppakgom.db.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class StudyAttend extends BaseEntity {
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "study_id")
+	Study study;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	User user;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "study_plan_id")
+	StudyPlan studyPlan;
+	
+	boolean isAttend;
+}
