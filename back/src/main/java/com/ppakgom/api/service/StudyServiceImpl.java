1593a5dac9b688b77b0ec7b990d1b1e9429dc57a
yallyyally <jhshin0028@gmail.com>
2021-08-12 16:46:52 +0900

    [BE][Feat][#S05P13B306-122] 디폴트 이미지 경로 변경
    image/study/default.png -> image/default.png

--- back/src/main/java/com/ppakgom/api/service/StudyServiceImpl.java
+++ back/src/main/java/com/ppakgom/api/service/StudyServiceImpl.java
@@ -76,7 +76,7 @@ public class StudyServiceImpl implements StudyService {
 	@Autowired
 	StudyTestRepository studyTestRepository;
 
-	String BASE_PATH = System.getProperty("user.dir") + "\\src\\main\\resources\\image\\study\\";
+	String BASE_PATH = System.getProperty("user.dir") + "\\src\\main\\resources\\image\\";
 
 	UserLikeStudyRepository userLikeStudyRepository;
 
@@ -106,13 +106,13 @@ public class StudyServiceImpl implements StudyService {
 		Long studyId = studyTmp.getId();
 
 //		 사진 관련 처리 -> image/study/방번호-파일명
-		String path = BASE_PATH;;
+		String path = BASE_PATH;
 		if(studyThumbnail == null) {
 //			디폴트 사진 처리
 			path += "default.png";
 		}
 		else {
-			path +=studyId + "-" + studyThumbnail.getOriginalFilename();
+			path +="study\\"+studyId + "-" + studyThumbnail.getOriginalFilename();
 			File dest = new File(path);
 			studyThumbnail.transferTo(dest);
 		}
@@ -479,13 +479,13 @@ public class StudyServiceImpl implements StudyService {
 //		썸네일 저장하기
 
 //		 사진 관련 처리 -> image/study/방번호-파일명
-		String path = BASE_PATH;;
+		String path = BASE_PATH;
 		if(studyThumbnail == null) {
 //			디폴트 사진 처리
 			path += "default.png";
 		}
 		else {
-			path +=study.getId() + "-" + studyThumbnail.getOriginalFilename();
+			path +="study\\"+study.getId() + "-" + studyThumbnail.getOriginalFilename();
 			File dest = new File(path);
 			studyThumbnail.transferTo(dest);
 		}
